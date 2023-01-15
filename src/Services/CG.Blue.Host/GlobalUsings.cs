2563adabff338bd3a68826521341247474b9c089
martycook <martinc@valornet.com>
2023-01-15 15:39:39 +0000

    CI Updates [skip ci]

--- src/Services/CG.Blue.Host/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using MudBlazor;
-global using MudBlazor.Services;
-global using CG.Validations;
-global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.AspNetCore.Mvc.RazorPages;
-global using System.Diagnostics;
